2ee8200c470ffc889763a3d5e79304ac361cb1c2
Khaled <kr72@outlook.fr>
2019-04-26 10:13:17 +0200

    Création du fichir carreNombre.js

---
+++ carreNombre.js
@@ -0,0 +1,21 @@
+//  Renvoie le carré d'un nombre
+function carre(x) {
+//TODO : compléter le code de la fonction
+	var produit = x*x ;
+	return produit;
+}
+	var nombre=Number(prompt("Saisissez un nombre !"));
+   // while (nombre != 0){
+    if (nombre = "0"){
+		console.log (" Le carre de " + nombre + " est : " + carre(nombre));
+	}
+	if (nombre = "2"){
+		console.log (" Le carre de " + nombre + " est : " + carre(nombre));
+	}
+    if (nombre = "5"){
+        console.log(" Le carre de " + nombre + " est : " + carre(nombre));
+    }
+    
+    else { 
+      console.log ("Recommencez !");
+    }
